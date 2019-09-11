d68811c3fa60b01bc7c4f155b3e6bcb022a9c6de
yx570 <16510404@qq.com>
2019-09-11 18:12:33 +0800

    修改文件名，解决大小写问题

--- web/static/js/page/login.js
+++ web/static/js/page/login.js
@@ -4,6 +4,18 @@
 		init: function () {
 			var _t = this;
 
+			form.on('submit(loginSubmit)', function () {
+				utils.alert('还未绑定微信，立即绑定', {}, function () {
+					utils.msg('绑定成功');
+					setTimeout(function () {
+						window.location = '/lawyer/auth.html';
+					}, 1000);
+				});
+			})
+
+			$('.wechatLogin').click(function () {
+				
+			});
 		}
 	}
 
