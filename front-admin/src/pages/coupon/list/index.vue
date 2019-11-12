f8bab5b43b2df32de64753af5890d998ea0381b9
yx570 <16510404@qq.com>
2019-11-13 00:28:39 +0800

    update table thead

--- front-admin/src/pages/coupon/list/index.vue
+++ front-admin/src/pages/coupon/list/index.vue
@@ -1,12 +1,214 @@
 <template>
-  <div class="await-dev">
-    <h1 class="cl-999">此功能正在努力开发中！！！</h1>
-  </div>
+  <el-row>
+    <el-card class="search-card">
+      <app-form
+        class="app-search"
+        :init="searchFormInit"
+        :items="searchItems"
+        :btns="searchBtns"
+        @confirm="confirmSearch"
+        @reset="confirmSearch"
+        ref="searchForm"
+      />
+    </el-card>
+    <el-card class="table-card mt-10">
+      <el-row slot="header" class="clearfix">
+        <el-row class="fl">
+          <span class="title">优惠券</span>
+        </el-row>
+        <el-row class="fr">
+          <el-button type="primary">新增</el-button>
+          <el-button type="primary">修改</el-button>
+          <el-button type="primary">删除</el-button>
+        </el-row>
+      </el-row>
+      <app-table 
+        ref="appTable"
+        url="/pc/coupon/pool"
+        columnType="selection"
+        :params="tableParams"
+        :columns="columns"
+        :columns-props="columnsProps"
+        @selection-change="tableSelect"
+      />
+    </el-card>
+    <app-dialog
+      :width="dialogWidth"
+      :height="dialogHeight"
+      :title="dialogTitle"
+      :visible="dialogVisible"
+      @close="closeDialog"
+    >
+      <component
+        v-if="dialogVisible"
+        :is="dialogComponent"
+        :row="dialogForm"
+        @submit="formSubmit"
+        @cancel="closeDialog"
+        ref="dialogComponent"
+      />
+    </app-dialog>
+  </el-row>
 </template>
 
-<style lang="less">
-  .await-dev {
-    line-height: 300px;
-    text-align: center;
+<script>
+  import { mapActions } from 'vuex'
+  import AppTable from '@/mixins/table'
+  import AppDialog from '@/mixins/dialog'
+  import AppSearch from '@/mixins/search'
+  import AppRsText from '@/components/app-table/lib/rsText'
+  export default {
+    components: {
+      // Detail: () => import("./detail"),
+      // HqDivide: () => import("./hqDivide"),
+    },
+    mixins: [AppTable, AppDialog, AppSearch],
+    data() {
+      return {
+        columns: [
+          {
+            label: '编号',
+            field: 'index',
+            width: 100
+          },{
+            label: '优惠券名称',
+            field: 'couponName',
+          },{
+            label: '优惠券种类',
+            field: 'scene',
+          },{
+            label: '类型',
+            field: 'type',
+          },{
+            label: '描述',
+            field: 'remark',
+          },{
+            label: '发放总数',
+            field: 'sendCount',
+          },{
+            label: '领取总数',
+            field: '',
+          },{
+            label: '使用总数',
+            field: '',
+          },{
+            label: '未使用总数',
+            field: '',
+          },{
+            label: '开始时间',
+            field: 'rangeStartTime',
+          },{
+            label: '结束时间',
+            field: 'rangeEndTime',
+          },{
+            label: '状态',
+            field: 'dataStatus',
+          },{
+            label: '操作',
+            field: 'operate',
+            align: 'center',
+            width: 120,
+            type: 'button',
+            default: '修改',
+            on: {
+              click: ({ row }) => {
+                this.handleBtnAction(row, 'edit')
+              }
+            }
+          }
+        ],
+        columnsProps: {
+          minWidth: 100,
+        }
+      }
+    },
+    methods: {
+      // 初始化页面
+      initPage() {
+        this.$set(this.searchFormInit, 'align', 'left')
+
+        this.searchItems = [
+          {
+            label: '优惠券种类',
+            field: 'scene',
+            type: 2,
+          },
+          {
+            label: '有效时间',
+            field: ['rangeStartTime', 'rangeEndTime'],
+            startPlaceholder: '开始时间',
+            endPlaceholder: '结束时间',
+            type: 10
+          },
+          {
+            label: '名称',
+            field: 'couponName',
+            type: 1,
+          },
+        ]
+
+        this.searchBtns = [
+          {
+            label: '确认',
+            code: 'confirm'
+          }, {
+            label: '重置',
+            code: 'reset',
+            type: 'default'
+          }
+        ]
+      },
+      // 表单提交
+      async formSubmit(form) {
+        try {
+          const searchForm = this.$refs.searchForm
+          const tenantId = this.$val(form, 'tenant.id')
+          let params = {
+            tenantId: this.curRow.id,
+            hqTenantId: form
+          }
+          switch (this.dialogComponent) {
+            case 'HqDivide':
+              await this.tenantDivideHq(params)
+              this.$msgSuccess('操作成功')
+              this.closeDialog()
+              this.refreshTable()
+              break;
+          }
+        } catch (e) {
+          // error
+        }
+      },
+      async handleBtnAction(row, type) {
+        let res = {}
+        switch (type) {
+          case 'detail':
+            this.dialogWidth = '800px'
+            this.dialogTitle = row.tenantName
+            res = await this.tenantView({ id: row.id })
+            this.dialogForm = res.data || {}
+            this.dialogComponent = 'Detail'
+            this.dialogVisible = true
+            break;
+          case 'hqDivide':
+            this.dialogWidth = '1000px'
+            this.dialogTitle = '选择总部'
+            res = await this.tenantView({ id: row.id })
+            this.curRow = res.data
+            this.dialogForm = res.data || {}
+            this.dialogComponent = 'HqDivide'
+            this.dialogVisible = true
+            break;
+        }
+      },
+      ...mapActions('tenant', [
+        'tenantView',
+        'tenantDivideHq',
+        'tenantGetKV'
+      ])
+    },
+    created() {
+      this.initPage()
+    }
   }
-</style>
+</script>
