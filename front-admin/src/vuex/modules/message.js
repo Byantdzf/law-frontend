29433127e29bab4a130a22dc44dc0c0aa71e5cfa
1056874343@qq.com <1056874343@qq.com>
2019-11-26 13:36:35 +0800

    fix bug message

--- front-admin/src/vuex/modules/message.js
+++ front-admin/src/vuex/modules/message.js
@@ -4,10 +4,10 @@ const message = {
   namespaced: true,
   actions: {
     // 咨询者会员 -- 详情
-    addMessage: (context, params) => post(`/mng/message/add`, params),
+    addMessage: (context, params) => post(`/mng/message/add`, params, {contentType: 'application/json; charset=utf-8'}),
     getMemberlist: (context, params) => get(`/mng/message/member/list`, params),
-    removeMessage: (context, ids) => del(`/mng/message/delete/${ids}`),
-    updateMessage: (context, params) => post(`/mng/message/update`, params),
+    removeMessage: (context, ids) => del(`/mng/message/delete/${ids}`,{},{contentType: 'application/json; charset=utf-8'}),
+    updateMessage: (context, params) => post(`/mng/message/update`, params,{contentType: 'application/json; charset=utf-8'}),
     getMessageDetail: (context, id) => get(`/mng/message/details/${id}`)
   }
 }
